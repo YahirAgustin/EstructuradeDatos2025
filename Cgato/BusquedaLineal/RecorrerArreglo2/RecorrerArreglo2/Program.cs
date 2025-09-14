5c27f7e821716fa70d5b19274f7668338115092f
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-14 00:06:44 -0700

    Add files via upload

---
+++ Cgato/BusquedaLineal/RecorrerArreglo2/RecorrerArreglo2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] Array = new int[5];
+
+        Array[0] = 2;
+        Array[1] = 4;
+        Array[2] = 6;
+        Array[3] = 8;
+        Array[4] = 10;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(Array[i]);
+        }
+
+        Console.ReadKey(); // Para pausar la consola
+    }
+}
+
