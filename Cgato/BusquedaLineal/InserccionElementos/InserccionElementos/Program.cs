b5de7713da4dece71fefb3c5f359bf61cc612091
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-14 00:05:40 -0700

    Add files via upload

---
+++ Cgato/BusquedaLineal/InserccionElementos/InserccionElementos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] Array = new int[5];
+
+        Array[0] = 2;
+        Array[1] = 4;
+        Array[2] = 6;
+        Array[3] = 8;
+        Array[4] = 10;
+
+        Console.WriteLine("Arreglo antes de insertar elemento:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(Array[i] + " ");
+        }
+        Console.WriteLine();
+
+        int Element = 9;
+        int Posicion = 3;
+        int Aux = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            if (Posicion == i)
+            {
+                Aux = Array[i];
+                Array[i] = Element;
+                Element = Aux;
+                Posicion += 1;
+            }
+        }
+
+        Console.WriteLine("Arreglo después de insertar elemento:");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write(Array[i] + " ");
+        }
+
+        Console.ReadKey(); // Pausar la consola
+    }
+}
