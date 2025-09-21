43448007c7783a3cc95338914f13b4faf1f1a29a
YahirAgustin <157630965+YahirAgustin@users.noreply.github.com>
2025-09-21 09:11:47 -0700

    Algoritmo Inserccion

---
+++ Cgato/BusquedaLineal/OrdenamientoInserccion/OrdenamientoInserccion/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new Random();
+        int[] arreglo = new int[10];
+
+      
+        for (int i = 0; i < 10; i++)
+        {
+            arreglo[i] = rand.Next(0, 101);
+        }
+
+        Console.WriteLine("Arreglo antes de ordenar:");
+        foreach (int num in arreglo)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+
+        for (int i = 1; i < arreglo.Length; i++)
+        {
+            for (int j = i; j > 0; j--)
+            {
+                if (arreglo[j] < arreglo[j - 1])
+                {
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j - 1];
+                    arreglo[j - 1] = aux;
+                }
+                else
+                {
+                    break; 
+                }
+            }
+        }
+
+        Console.WriteLine("Arreglo ordenado:");
+        foreach (int num in arreglo)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
